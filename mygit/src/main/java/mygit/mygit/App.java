46a47ed25b16908b48373d2dff3f29f86ea86d6e
tgoswami <goswami.tarun77@gmail.com>
2020-03-06 13:21:34 +0530

    My First Commit

---
+++ mygit/src/main/java/mygit/mygit/App.java
@@ -0,0 +1,13 @@
+package mygit.mygit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
